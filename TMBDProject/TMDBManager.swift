cd81d4d1e4a5718824e9803b7b59608e4f8c74e4
Jay <105216574+JaeBeen0725@users.noreply.github.com>
2023-11-05 15:14:11 +0900

    기본파일 생성

---
+++ TMBDProject/TMDBManager.swift
@@ -0,0 +1,8 @@
+//
+//  TMDBManager.swift
+//  TMBDProject
+//
+//  Created by Jae Oh on 11/5/23.
+//
+
+import Foundation
---
+++ TMBDProject/TMDBOnboardingViewController.swift
@@ -0,0 +1,8 @@
+//
+//  TMDBOnboardingViewController.swift
+//  TMBDProject
+//
+//  Created by Jae Oh on 11/5/23.
+//
+
+import Foundation
